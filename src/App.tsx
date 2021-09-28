1d2cb588ec56d8deba59a198de454acff91825fe
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2021-09-29 01:43:05 +0200

    Inital Commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { useState } from "react";
+import { Context } from "./Context";
+import Home from "./Home";
+
+function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://vwlyc.sse.codesandbox.io/",
+  });
+
+  const [page, setPage] = useState("1");
+  const [name, setName] = useState("");
+
+  return (
+    <Context.Provider value={{ page, name, setPage, setName }}>
+      <ApolloProvider client={client}>
+        <Home />
+      </ApolloProvider>
+    </Context.Provider>
+  );
+}
+
+export default App;
